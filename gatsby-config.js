c3327e9b73586869109f5dd6653cd4d13d6d8a6e
Dawnabelle <dawn.mott@gmail.com>
2019-09-11 16:36:58 -0700

    create and build gatsby project

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  plugins: [
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `markdown-pages`,
+      path: `${__dirname}/src/markdown-pages`,
+    },
+  },
+  `gatsby-transformer-remark`,
+]
+}
