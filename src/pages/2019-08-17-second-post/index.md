711c7286a5b532d603ea690852fc9e0dc395fb79
Dawnabelle <dawn.mott@gmail.com>
2019-09-19 21:11:32 -0700

    remove commented out code

--- src/pages/2019-08-17-second-post/index.md
+++ src/pages/2019-08-17-second-post/index.md
@@ -5,13 +5,6 @@ title: "Fear of Migrating Florida Citizens Could Bring About Climate Change"
 tags: ['Florida', 'climate change']
 excerpt: "Do you want Floridians in your town?"
 ---
+## Floridians
 
-The fact that Florida will be underwater and will displace Floridians to other places in the country and the world should be enough to get any American/global citizen to fight climate change
-
-## Test TitleAndDescription
-
-- a list
-- is asweome
-- I can't spell
-
->>> Blockquote
+>>> The fact that Florida will be underwater and will displace Floridians to other places in the country and the world should be enough to get any American/global citizen to fight climate change
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -4,8 +4,7 @@ import { graphql, Link } from "gatsby"
 export default function Template(props) {
   const { pageContext = {} } = props;
   const { date = "2020", html = "<p>There was an error</p>", pathSlug, title } = pageContext;
-  // const { markdownRemark } = data // data.markdownRemark holds our post data
-  // const { frontmatter, html } = markdownRemark
+
   return (
     <div className="blog-post-container" style={{
         display: 'flex',
@@ -26,16 +25,3 @@ export default function Template(props) {
     </div>
   )
 }
-
-// export const pageQuery = graphql`
-//   query($path: String!) {
-//     markdownRemark(frontmatter: { path: { eq: $path } }) {
-//       html
-//       frontmatter {
-//         date(formatString: "MMMM DD, YYYY")
-//         path
-//         title
-//       }
-//     }
-//   }
-// `
